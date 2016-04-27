398fdf23dabe9d9c211a2c7ac212ce6871494946
Ninos Ego <me@ninosego.de>
2016-04-28 01:06:54 +0200

    Added: frontpage

--- bewegdich/polls/urls.py
+++ bewegdich/polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^main/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^map/$', views.map, name='map'),
     url(r'^login/$', views.login_user, name='login_user'),
     url(r'^form/$', views.get_dest, name='form'),
