7a867105b97b7d2e7f2d869e0f2b72d7b8267d82
Ninos Ego <me@ninosego.de>
2016-06-06 17:41:06 +0200

    Closed #21

--- src/static/assets/js/app.js
+++ src/static/assets/js/app.js
@@ -8,17 +8,37 @@ var App = {
             serviceUrl: '/api/getStopList/',
             dataType: 'json',
             paramName: 'query',
+            params: {
+                longitude: function () {
+                    return AppLocation.position.longitude;
+                },
+                latitude: function () {
+                    return AppLocation.position.latitude;
+                }
+            },
             transformResult: function (response) {
                 return {
                     query: response.query,
                     suggestions: jQuery.map(response.suggestions, function (dataItem) {
-                        return {value: dataItem.value.toString(), data: dataItem.data};
+                        return {value: dataItem.data, data: dataItem.value.toString()};
                     })
                 };
             },
             onSelect: function (suggestion) {
-                alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+                jQuery(document).trigger('App.destination.selected', [suggestion]);
             }
         });
+
+        this.hooks();
+    },
+    hooks: function () {
+        var that = this;
+
+        jQuery(document).on('App.destination.selected', function (event, suggestion) {
+            that.getRoute(suggestion.data);
+        });
+    },
+    getRoute: function (destination) {
+        //alert(destination);
     }
 };
