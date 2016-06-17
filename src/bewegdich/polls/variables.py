68f861bfd8ecfeb7e63f8c7913b1d758f5434df7
Matthias Krempel <matthias.krempel@hs-augsburg.de>
2016-06-17 09:44:59 +0200

    Changed name

--- src/bewegdich/polls/variables.py
+++ src/bewegdich/polls/variables.py
@@ -6,4 +6,4 @@ This file contains all variables and constants which are the same for every user
 #Keys for the usersession object
 
 SPEED = "speed" #  The wakling speed of the user
-LOCATION = "ip_location" #  The coordinates of the user
+LOCATION = "location" #  The coordinates of the user
